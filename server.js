9a788ce4e0658d0421d2df55a6d9c19e0dd59d75
Jibola007 <ajibbola007@gmail.com>
2025-08-27 15:16:32 +0100

    db

--- server.js
+++ server.js
@@ -21,13 +21,20 @@ app.use('/api/auth', authRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("✅ Connected to MongoDB");
 
-  .then(() => {
-    const uploadPath = 'uploads';
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath);
-}
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error('MongoDB connection error:', err));
+  const uploadPath = "uploads";
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath);
+  }
+
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on port ${PORT}`)
+  );
+})
+.catch((err) => console.error("🔥 MongoDB connection error:", err));
